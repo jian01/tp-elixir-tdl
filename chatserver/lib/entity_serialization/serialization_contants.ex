c1fd7d24802384b7230a840b57d8d9afa5ac149a
Gianmarco Cafferata <giancafferata@hotmail.com>
2021-01-28 20:49:10 -0300

    Issue with gitignore

---
+++ chatserver/lib/entity_serialization/serialization_contants.ex
@@ -0,0 +1,7 @@
+defmodule SerializationConstants do
+  use Constants
+
+  const :new_message_type, "new_message"
+  const :receipt_notice_type, "receipt_notice"
+  const :text_message_type, "text"
+end
