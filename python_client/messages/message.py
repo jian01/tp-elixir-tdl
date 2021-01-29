b38966edde33c47a06af615f06d00dc729b62ae7
Gianmarco Cafferata <giancafferata@hotmail.com>
2021-01-29 20:00:22 -0300

    Bug fixed

--- python_client/messages/message.py
+++ python_client/messages/message.py
@@ -17,12 +17,15 @@ class Message:
     recipient: int
     content: str
 
-    def __init__(self, sender: int, recipient: int, message_id: Optional[int] = None):
+    def __init__(self, sender: int, recipient: int,
+                 message_id: Optional[int] = None,
+                 creation_datetime: Optional[datetime] = None):
         """
 
         :param message_id: the id of the message
         :param sender: sender of the message
         :param recipient: recipient of the message
+        :param creation_datetime: the created datetime
         """
         if not message_id:
             self.message_id = Message.MESSAGE_ID_COUNT
@@ -31,7 +34,18 @@ class Message:
             self.message_id = message_id
         self.sender = sender
         self.recipient = recipient
-        self.created_datetime = datetime.now()
+        if not creation_datetime:
+            self.created_datetime = datetime.now()
+
+    @staticmethod
+    def deserialize_content(content):
+        """
+        Deserializes the content of the message if needed
+
+        :param content: the content to deserialize
+        :return: the object
+        """
+        return content
 
     @abstractmethod
     def serialize(self) -> str:
@@ -49,7 +63,8 @@ class Message:
         """
         types = {cls.SERIALIZER_NAME: cls for cls in Message.__subclasses__()}
         data_dict = json.loads(serialized)
-        msg = types[data_dict['type']](data_dict['sender'], data_dict['recipient'], data_dict['id'])
-        msg.content = data_dict['content']
-        msg.created_datetime = dateutil.parser.parse(data_dict['created_datetime'])
+        content = types[data_dict['type']].deserialize_content(data_dict['content'])
+        msg = types[data_dict['type']](data_dict['sender'], data_dict['recipient'],
+                                       content, data_dict['id'],
+                                       dateutil.parser.parse(data_dict['created_datetime']))
         return msg
